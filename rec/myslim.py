b40126983729eafcdc4da211303d33cb016c7f44
YuyangZhang <zhangyuyang4d@163.com>
2017-10-22 22:11:48 +0800

    2017-10-22 Update SLIM

--- rec/myslim.py
+++ rec/myslim.py
@@ -1,20 +1,47 @@
-from scipy.sparse import lil_matrix
+from scipy.sparse import lil_matrix, csr_matrix, csc_matrix
 
 
-class sparse_matrix(lil_matrix):
+class SparseMatrix(lil_matrix):
 
-	def __init__(self, arg1, shape=None, dtype=None, copy=False):
-		lil_matrix.__init__(arg1, shape=shape, dtype=dtype, copy=copy)
+    def lil_get_col_to_csc(self, j):
+        row_num, col_num = self.shape
+        new_mat = lil_matrix((row_num, col_num - 1), dtype=self.dtype)
+        new_col = self.getcol(j)
+        new_mat[:, :j] = self[:, :j]
+        new_mat[:, j:] = self[:, j + 1:]
+        return csc_matrix(new_col), csc_matrix(new_mat)
 
-	def get_row_other(self,i):
-		i = sp.lil_matrix._check_row_bounds(i)
-		
-		new_row = lil_matrix((1, self.shape[1]), dtype=self.dtype)
-		new_row.rows[0] = self.rows[i][:]
-		new_row.data[0] = self.data[i][:]
-		
-		new_matrix = lil_matrix((self.shape[0]-1, self.shape[1]), dtype=self.dtype)
-		new_matrix.rows[:i] = self.rows[:i][:]
-		new_matrix.data[:i] = self.data[:i][:]
-		new_matrix.row[i:] = self.rows[i+1:][:]
-		new_matrix.data[i:] = self.data[i+1:][:]
+    def lil_get_row_to_csc(self, i):
+        row_num, col_num = self.shape
+        new_mat = lil_matrix((row_num - 1, col_num), dtype=self.dtype)
+        new_row = self.getrow(i)
+        new_mat[:i, :] = self[:i, :]
+        new_mat[i:, :] = self[i + 1:, :]
+        return csc_matrix(new_row), csc_matrix(new_mat)
+
+    def get_row_other(self, i):
+        new_row = lil_matrix((1, self.shape[1]), dtype=self.dtype)
+        new_row.rows[0] = self.rows[i][:]
+        new_row.data[0] = self.data[i][:]
+
+        new_matrix = lil_matrix(
+            (self.shape[0] - 1, self.shape[1]), dtype=self.dtype)
+        new_matrix.rows[:i][:] = self.rows[:i][:]
+        new_matrix.data[:i][:] = self.data[:i][:]
+        new_matrix.rows[i:][:] = self.rows[i + 1:][:]
+        new_matrix.data[i:][:] = self.data[i + 1:][:]
+        return csr_matrix(new_row), csr_matrix(new_matrix)
+
+    def get_col_other(self, j):
+
+        return None
+
+
+a = SparseMatrix((10, 10))
+a[1, 1] = 1
+b, c = a.lil_get_col_to_csc(1)
+d, e = a.lil_get_row_to_csc(1)
+print(b.shape)
+print(c.shape)
+print(d.shape)
+print(e.shape)
