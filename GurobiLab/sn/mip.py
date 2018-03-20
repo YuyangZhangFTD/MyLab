e13065da0e9050e72ce8935b8df6362d1069f248
YuyangZhang <zhangyuyang4d@163.com>
2018-03-20 22:04:45 +0800

    2018-03-20

---
+++ GurobiLab/sn/mip.py
@@ -0,0 +1,56 @@
+import numpy as np
+from gurobipy import *
+
+
+c = np.loadtxt("c.csv", delimiter="\t")
+od_wgt = np.loadtxt("od_wgt.csv", delimiter=",")
+max_wgt = np.loadtxt("max_wgt.csv", delimiter=",")
+od1 = np.loadtxt("od1.csv", delimiter=",")
+od2 = np.loadtxt("od2.csv", delimiter=",")
+
+num_i, num_j = c.shape
+Inf = 1e8
+loading_rate = 0.5
+
+# model
+m = Model()
+
+# add variables
+x = m.addVars([i for i in range(num_i)],[j for j in range(num_j)], vtype=GRB.INTEGER)
+q = m.addVars([j for j in range(num_j)], vtype=GRB.INTEGER)
+
+# set objective
+m.setObjective(quicksum(x[i,j]*c[i,j] for i in range(num_i) for j in range(num_j)), GRB.MAXIMIZE)
+
+# add constraints
+# x < cij * inf
+m.addConstrs((
+    x[i, j] <= c[i,j] * Inf
+    for i in range(num_i)
+    for j in range(num_j)
+))
+# \sum_j x <= od_wgt_i
+m.addConstrs((
+    quicksum(x[i,j] for j in range(num_j)) <= od_wgt[i] 
+    for i in range(num_i)
+))
+# od1
+m.addConstrs((
+    quicksum(x[i,j] for i in od1[j,:]) <= max_wgt[j] * q[j]
+    for j in range(num_j)
+))
+m.addConstrs((
+    quicksum(x[i,j] for i in od1[j,:]) >= max_wgt[j] * (q[j] - 1 + loading_rate)
+    for j in range(num_j)
+))
+# od2
+m.addConstrs((
+    quicksum(x[i,j] for i in od2[j,:]) <= max_wgt[j] * q[j]
+    for j in range(num_j)
+))
+m.addConstrs((
+    quicksum(x[i,j] for i in od2[j,:]) >= max_wgt[j] * (q[j] - 1 + loading_rate)
+    for j in range(num_j)
+))
+
+print(m.optimize())
